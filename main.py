60cc5c50c8b445d26e1239dc0115e7f6ee4a177f
whyireland <fqckyouboi@gmail.com>
2019-04-06 13:12:43 +0000

    Now Takes Args!!!!

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import discord
 import asyncio
+import sys
 
-# put your bot's token here VV
-TOKEN = "Put your bot's token here :)"
-
-# a = time till messages get deleted
-a = 1600
+for a in sys.argv[1:]:
+    if a.startswith("token:"):
+        TOKEN=str(a[6:])
+        print("token:", TOKEN)
+    elif a.startswith("time:"):
+        try:
+            time=int(a[5:])
+            print("time:", time)
+        except:
+            print("ERROR: Time must be an int")
+            input()
+    elif a.startswith("playing:"):
+        pStatus=a[8:]
+        print("playing:", pStatus)
+    else:
+        print("""not recognised {}:
+        --HELP--
+    -ARGS-
+'playing:'  |   set playing status
+'time:'     |   set time to delete messages in secs
+'token:'    |   bot token""".format(a))
+        exit()
 
 
 client=discord.Client()
@@ -13,20 +31,17 @@ client=discord.Client()
 @client.event
 async def on_message(message):
     if message.author!=client.user:
-        await client.change_presence(game=discord.Game(name="Deleting old messages! || "))
-        await asyncio.sleep(a)
-        #await client.send_message(client.get_channel("563447331844915220"), "```\n     BOT-LOG\n-----------------\n|deleted message|\n-----------------```{}\n\n{}\n\n{}".format(message.content)
+        await client.change_presence(game=discord.Game(name=pStatus))
+        await asyncio.sleep(time)
         await client.delete_message(message)
         print("deleted message!")
 
 @client.event
 async def on_ready():
     print("Logged in as: {} || {}".format(client.user.name, client.user.id))
+    print("     BOT ONLINE!")
 
 
-if TOKEN != "Put your bot's token here :) ":
-    client.run(TOKEN)
-else:
-    print("Cheange the 'TOKEN' variable!")
+client.run(TOKEN)
 
 #https://discordapp.com/oauth2/authorize?client_id=563430890462773263&permissions=8&scope=bot
